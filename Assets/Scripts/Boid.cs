6545d809c2212f25c8043c41f1cb04f228551142
Soshi H <soshi@lolol.ooo>
2022-08-03 15:23:53 +0900

    renamed class

---
+++ Assets/Scripts/Boid.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boids : MonoBehaviour
+{
+
+    BoidSettings settings;
+
+    // State
+    [HideInInspector]
+    public Vector3 position;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
